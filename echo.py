5f2e8b57e39729399228607f212d95da6904ec94
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-04 16:17:21 -0500

    got upper functioning

--- echo.py
+++ echo.py
@@ -1,22 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""An enhanced version of the 'echo' cmd line utility"""
+"""An enhanced version of the "echo" cmd line utility"""
 
-__author__ = "???"
+__author__ = "mhoelzer"
 
 
 import sys
+import argparse
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="change text based on command line input/option")
+    # parser.add_argument("-h", "--help", help="show this help message and exit")
+    parser.add_argument(
+        "-u", "--upper", help="convert text to uppercase", action="store_true")
+    parser.add_argument(
+        "-l", "--lower", help="convert text to lowercase", action="store_true")
+    parser.add_argument(
+        "-t", "--title", help="convert text to titlecase", action="store_true")
+    parser.add_argument("text", help="text to be manipulated")
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    print("main args are {}".format(args))
+    parser = create_parser()
+    # if not arguments
+    if not args:
+        # ^^^ aka if len(sys.argv) <= 1
+        parser.print_usage()
+        sys.exit(1)
+    namespace = parser.parse_args(args)
+    output_text = namespace.text
+    # do the bussiness logic of echo.py; transform
+    if namespace.upper:
+        output_text = output_text.upper()
+    if namespace.lower:
+        output_text = output_text.lower()
+    if namespace.title:
+        output_text = output_text.title()
+    return output_text
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    # example of cmdl: python echo.py --upper "Michael was Here"
+    print("original sys.argv is {}".format(sys.argv))
+    print(main(sys.argv[1:]))
+    # ^^^ get everything after the file name
+    # ^^^ reference a slice of a list, so to get another list of just the
+    # user-supplied arguments (but without the script name)
+    # ^^^ first arg after python is the echo.py but dont give main func the
+    # echo.py and main wants the last 2 args
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -3,9 +3,66 @@
 
 import unittest
 import echo
+# ^^^ this is the echo file
+import subprocess
+
 
 # Your test case class goes here
+class TestEcho(unittest.TestCase):
+    # ^^^ we get helpful asserts from ut
+    """this tests all the args variants of echo.py"""
+
+    # call func, get params, test to see if passse, then pass/fail
+    def test_help(self):
+        """Running the program without arguments should show usage."""
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect its output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+        self.assertEquals(stdout, usage)
+
+    # def test_upper(self):
+    #     process = subprocess.Popen(
+    #         ["python", "./echo.py", "-u"],
+    #         stdout=subprocess.PIPE)
+    #     stdout, _ = process.communicate()
+    #     # self.assertEquals(stdout, stdout)
+    #     self.assertTrue(stdout.upper())
+    #     # creatparser and make obj; feed args into; parser.parseargs and see
+    #     # if an arg is a -u, namespace.upper is true
+    
+    # test the -u option 
+    def test_upper_short(self):
+        args = ["-u" or "--upp"]
+        # ^^^ pass into a funciton and get transformed; parser will get -u and
+        # translate string to uppercase and send back
+        # test whether parser detects the -u flag and makes ns attr of ns.upper
+        parser = echo.create_parser()
+        namespace = parser.parse_args(args)
+        print("namespace is {}".format(namespace))
+        self.assertTrue(namespace.upper)
+        result = echo.main(args)
+        self.assertEqual(result, "MICHAEL WAS HERE")
+
+    # def test_upper_long(self):
+    #     args = ["--upper", "Michael ewas here"]
+    #     parser = echo.create_parser()
+    #     namespace = parser.parse_args(args)
+    #     print("namespace is {}".format(namespace))
+    #     self.assertTrue(namespace.upper)
+    #     result = echo.main(args)
+    #     self.assertEqual(result, "MICHAEL WAS HERE")
+        # use python -m unittest discover to test the add-ins here
+
+    def test_lower(self):
+        pass
+
+    def test_title(self):
+        pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
